c7ff9471ca0682470e6b994043f2868e31ae0d70
Evgeniy Tolochkin <djon-rap@yandex.ru>
2022-07-21 22:27:37 +1000

    ПР 11

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-    <p className="footer__author">
-      &copy; 2022 Evgeniy Tolochkin. Яндекс Практикум
+      <p className="footer__author">
+        &copy; 2022 Evgeniy Tolochkin. Яндекс Практикум
     </p>
     </footer>
   )
